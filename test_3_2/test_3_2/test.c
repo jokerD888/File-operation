889325e9dd1e6155aa1e59347c2dc50d27ebb64f
joker <1783779005@qq.com>
2021-03-04 21:16:08 +0800

    文件操作

--- test_3_2/test_3_2/test.c
+++ test_3_2/test_3_2/test.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 #include<errno.h>
 #include<string.h>
-
+#include <stdlib.h>
 //int main(void)
 //{
 //	FILE* pfWrite = fopen("test.txt", "w");
@@ -194,18 +194,85 @@ struct S
 //	pf = NULL;
 //	return 0;
 //}
+//int main(void)
+//{
+////	struct S s = { "张三",20,55.6 };
+//	struct S tmp = { 0 };
+//	FILE* pf = fopen("test.txt", "rb");
+//	if (pf == NULL)
+//	{
+//		return 0;
+//	}
+//	//二进制的形式读文件
+//	fread(&tmp, sizeof(struct S), 1, pf);
+//	printf("%s %d %f\n", tmp.name, tmp.age, tmp.score);
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	FILE* pf = fopen("test.txt", "r");
+//	if (pf == NULL)
+//	{
+//		return 0;
+//	}
+//	//定位文件指针
+//	//fseek(pf, 2, SEEK_CUR);//文件当前
+//	fseek(pf, 2, SEEK_SET);//文件起始
+//	//fseek(pf, -2, SEEK_END);//文件末尾
+//	//读取文件
+//	int ch = fgetc(pf);
+//	printf("%c\n", ch);
+//
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	FILE* pf = fopen("test.txt", "r");
+//	if (pf == NULL)
+//	{
+//		return 0;
+//	}
+//	//fseek(pf, -2, SEEK_END);
+//	fgetc(pf);
+//	int pos = ftell(pf);//返回文件指针相对于起始位置的偏移量
+//	printf("%d\n", pos);
+//	rewind(pf);//让文件指针的位置回到文件的起始位置
+//	pos = ftell(pf);
+//	printf("%d\n", pos);
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
 int main(void)
 {
-//	struct S s = { "张三",20,55.6 };
-	struct S tmp = { 0 };
-	FILE* pf = fopen("test.txt", "rb");
-	if (pf == NULL)
+	int c;
+	FILE* pf = fopen("test.txt", "r");
+	if (!pf)
 	{
-		return 0;
+		perror("File opening failed");
+		return EXIT_FAILURE;//返回-1
 	}
-	//二进制的形式读文件
-	fread(&tmp, sizeof(struct S), 1, pf);
-	printf("%s %d %f\n", tmp.name, tmp.age, tmp.score);
+	//fgetc当读取失败时或者遇到文件结束时，都会返回EOF
+	while ((c = fgetc(pf)) != EOF)
+	{
+		putchar(c);
+	}
+	//判断是什么原因结束的
+	if (ferror(pf))
+	{
+		puts("I/O error when reading");
+	}
+	else if(feof(pf))
+	{
+		puts("End of file reached successfully");
+	}
 	fclose(pf);
 	pf = NULL;
 	return 0;
